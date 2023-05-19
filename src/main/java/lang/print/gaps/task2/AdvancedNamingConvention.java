26711093de97fe49c22856fa1dd6cb78696567b4
m.yuldashev <yuldashevmuhiddin65@gmail.com>
2023-05-19 12:41:07 +0500

    bug fixed!

---
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -0,0 +1,13 @@
+package lang.print.gaps.task2;
+
+public class AdvancedNamingConvention {
+    private static final int ADULT_AGE = 18;
+    private int age;
+
+    void callToFriend() {
+
+    }
+    void callByNumbber(int number) {
+
+    }
+}
