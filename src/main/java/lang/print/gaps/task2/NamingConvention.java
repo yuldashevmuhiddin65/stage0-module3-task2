43075afeafdf9fbbfd79d1a063ace1d25ff034db
m.yuldashev <yuldashevmuhiddin65@gmail.com>
2023-05-19 12:26:27 +0500

    fixed!

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int variableTen = 10;
+
         int variableOne = 1;
         int variableTwo = 2;
         int variableThree = 3;
@@ -11,5 +11,7 @@ public class NamingConvention {
         int variableSix = 6;
         int variableSeven = 7;
         int variableEight = 8;
+        int variableNine = 9;
+        int variableTen = 10;
     }
 }
